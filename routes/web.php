f00c0c3a0ff055c488c3f8149de75460c61e85ea
fre2mansur <fre2mansur@gmail.com>
2019-06-10 14:33:16 +0530

    dashboard page restricted for logged users

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/user/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/user/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
